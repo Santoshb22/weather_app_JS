3b9658e9a936e3fa65b587f2f0714b53a0ab8b31
Santosh Bhardwaj <sb289848@gmail.com>
2023-11-28 11:12:02 +0530

    image updated

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const apiKey = "bb5f13072f084a2bb89142934232711";
+//weatherapi.com
 
 const searchBtn= document.querySelector(".input-search button")
 const input = document.querySelector(".input-search input")
@@ -7,6 +8,7 @@ const nameCity = document.querySelector(".city h2")
 const country = document.querySelector(".city p")
 const humidityIs = document.querySelector("#humidity-js")
 const windSpeed = document.querySelector("#wind-speed")
+const weatherImg = document.querySelector(".weather-details img")
 
 const showData = async (city) => {
 
@@ -21,6 +23,21 @@ nameCity.innerHTML = data.location.name
 country.innerHTML = `Country: ${data.location.country} and region: ${data.location.region}`
 humidityIs.innerHTML = data.current.humidity
 windSpeed.innerHTML = data.current.wind_kph + "km/h"
+
+//showing weather image according to weather condition 
+if (data.current.condition.text === "Mist") {
+      weatherImg.src = "./img/mist.png"
+} else if(data.current.condition.text === "Clouds" || "Partly cloudy"){
+    weatherImg.src = "./img/clouds.png"
+} else if(data.current.condition.text === "Clear"){
+    weatherImg.src = "./img/clear.png"
+} else if(data.current.condition.text === "Rain"){
+    weatherImg.src = "./img/rain.png"
+} else if(data.current.condition.text === "Drizzle"){
+    weatherImg.src = "./img/dizzle.png"
+} else if(data.current.condition.text === "Snow") {
+    weatherImg.src = "./img/snow.png"
+}
 
 console.log(data)
 }
